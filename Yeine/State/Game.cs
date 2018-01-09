509aace897f3198939b50e098b400a14e0fb54be
Thomas Castiglione <castiglione@mac.com>
2018-01-09 23:06:24 +0800

    prune usings

--- Yeine/State/Game.cs
+++ Yeine/State/Game.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using Yeine.State;
 
 namespace Yeine.State
 {
--- Yeine/Strategies/AlwaysPass.cs
+++ Yeine/Strategies/AlwaysPass.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using Yeine.API;
 using Yeine.State;
 
